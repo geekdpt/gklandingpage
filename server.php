be7ab72ed43529dad569c363da959ba187fa6328
Jérôme Schaeffer <jer.schaeffer@gmail.com>
2018-06-05 14:44:52 +0200

    fix slamp dep + phpcs + todo

--- server.php
+++ server.php
@@ -3,29 +3,30 @@
 use Aerys\{Host, Request, Response};
 use function Aerys\{root, router};
 use Functional as λ;
-use Slamp\SlackObject\Channel;
+use Slamp\Slack\Channel;
 
 require __DIR__.'/vendor/autoload.php';
 
-
-function respond(Response $res, $body, int $status = 200) {
+function respond(Response $res, $body, int $status = 200)
+{
     $res->setStatus($status);
     $res->addHeader('Content-Type', 'application/json');
     $res->end(json_encode($body));
 }
 
+//TODO encrypt slack token and get it from a file (travis)
 $geekdpt = new Slamp\WebClient(getenv('SLACK_TOKEN'));
 
 $router = router();
 
-$router->get('/team-info', function(Request $req, Response $res) use($geekdpt) {
+$router->get('/team-info', function (Request $req, Response $res) use ($geekdpt) {
     $chans = yield $geekdpt->channels->listAsync(['exclude_archived' => true]);
 
-    $chans = λ\sort($chans, function(Channel $chanA, Channel $chanB) {
+    $chans = λ\sort($chans, function (Channel $chanA, Channel $chanB) {
         return $chanB->getMembersCount() <=> $chanA->getMembersCount();
     });
 
-    $chans = λ\map($chans, function(Channel $chan) {
+    $chans = λ\map($chans, function (Channel $chan) {
         return [
             'name' => $chan->getName(),
             'membersCount' => $chan->getMembersCount(),
@@ -36,12 +37,12 @@ $router->get('/team-info', function(Request $req, Response $res) use($geekdpt) {
     respond($res, ['channels' => $chans]);
 });
 
-$router->post('/get-invite', function(Request $req, Response $res) use($geekdpt) {
+$router->post('/get-invite', function (Request $req, Response $res) use ($geekdpt) {
     $body = json_decode(yield $req->getBody());
 
     try {
         yield $geekdpt->users->admin->inviteAsync($body->email);
-    } catch(Exception $ex) {
+    } catch (Exception $ex) {
         respond($res, ['error' => $ex->getMessage()], 400);
         return;
     }
@@ -50,20 +51,22 @@ $router->post('/get-invite', function(Request $req, Response $res) use($geekdpt)
 });
 
 $host = (new Host)
-    ->expose('127.0.0.1', 8080)
+    ->expose('0.0.0.0', 8080) //TODO put this in config
     ->name('localhost')
     ->name('geekdpt.io')
     ->use($router)
     ->use(root(__DIR__.'/web/dist'));
 
-Amp\run(function() use($host) {
+Amp\run(function () use ($host) {
     $logger = new class extends Aerys\Logger {
-        protected function output(string $message) {
+        protected function output(string $message)
+        {
+            //TODO print log in a file
             print "$message\n"; // log to stdout
         }
     };
 
-    $server = (new Aerys\Bootstrapper(function() use ($host) {
+    $server = (new Aerys\Bootstrapper(function () use ($host) {
         return [$host];
     }))->init($logger, []);
 
